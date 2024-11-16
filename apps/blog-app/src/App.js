22019b9a799b17bc0c39a192e618fb3a00fc6d4b
Raihan-009 <islamraihan009@gmail.com>
2024-11-16 09:08:03 +0600

    blog-app added

---
+++ apps/blog-app/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div className="min-h-screen w-full bg-blue-500 flex items-center justify-center">
+      <div className="text-white text-4xl font-bold">
+        Blog application
+      </div>
+    </div>
+  );
+};
+
+export default App;
